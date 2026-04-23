b3c6f4f143a69bf5ae8f3adb72357bde8e593eeb
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-04-24 00:05:27 +0530

    Added largestelement file

--- largestelement.java
+++ largestelement.java
@@ -1,13 +1,13 @@
-public class largestelement{
-    public static void main(String[]args){
-        int[] arr ={1,2,70,76,6};
-        int max = 0;
-        for(int i = 0; i<arr.length; i++){
+public class largestelement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 70, 76, 6};
+        int max = arr[0];   
+        for(int i = 0; i < arr.length; i++){
             if(arr[i] > max){
                 max = arr[i];
-                
             }
         }
+
         System.out.println(max);
     }
 }
