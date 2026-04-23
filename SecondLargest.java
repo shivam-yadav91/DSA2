c95ee5b31cf093b1e66e66451d390da22bd3168a
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-04-24 00:04:29 +0530

    Added SecondLargest file

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,20 +1,18 @@
-public class SecondLargest {
-    public static void main(String[] args) {
-
-        int[] arr = {8, 8, 7, 6, 5};
+public class SecondLargest{
+    public static void main(String[]agrs){
+        int[] arr =  { 329,394,300,120};
         int max = Integer.MIN_VALUE;
         int secondmax = Integer.MIN_VALUE;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] > max) {
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max){
+                secondmax = max;
                 max = arr[i];
             }
-            else if (arr[i] < max && arr[i] > secondmax) {
+
+            else if(arr[i]< max && arr[i]> secondmax){
                 secondmax = arr[i];
             }
         }
-
         System.out.println(secondmax);
     }
 }
