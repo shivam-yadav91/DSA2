ff8ab016058bf538c6b855ad78feac6f4060ba90
shivam-yadav91 <shivam.yadav_bca24@gla.ac.in>
2026-04-26 23:54:49 +0530

    Added Issroted2 file

---
+++ Issorted2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Issorted2 {
+
+    public boolean isSorted(ArrayList<Integer> arr){
+        for(int i = 0; i < arr.size() - 1; i++){
+            if(arr.get(i) > arr.get(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(1);
+        arr.add(3);
+        arr.add(2);
+
+        Issorted obj = new Issorted();
+        boolean result = obj.isSorted(arr);
+
+        System.out.println(result);
+    }
+}
+
+
+
